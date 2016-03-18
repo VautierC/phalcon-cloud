bd3ae89965ae2319605c751804db7ee09a2c8de3
Clément Vautier <clement.vautier@etu.unicaen.fr>
2016-03-18 12:47:37 +0100

    20160318-12:46

--- app/controllers/MyDisquesController.php
+++ app/controllers/MyDisquesController.php
@@ -7,38 +7,44 @@ class MyDisquesController extends \ControllerBase {
 	public function indexAction(){
 		$cpt=0;
 		$user=Auth::getUser($this);
-		$disquesUser=Disque::find(array(
+		if(!is_null($user)){
+			$disquesUser=Disque::find(array(
 			"conditions" => "idUtilisateur = :id:",
 			"bind" => array("id" => $user->id)));
-		$listGroup=$this->jquery->bootstrap()->htmlListgroup("list",array());
-		foreach($disquesUser as $disque){
-			$occupation=ModelUtils::getDisqueOccupation($this->config->cloud,$disque);
-			$tarif=Tarif::findFirst(ModelUtils::getDisqueTarif($disque));
-			$cap=ModelUtils::sizeConverter($tarif->getUnite())*$tarif->getQuota();
-			$usage=round((($occupation/ModelUtils::sizeConverter($tarif->getUnite()))/$tarif->getQuota()),2);
-			if($usage<0.1)$style="info";
-			else if($usage<0.5)$style="success";
-			else if($usage<0.8)$style="warning";
-			else $style="danger";
 
-			$bootstrap=$this->jquery->bootstrap();
-			$bt=$bootstrap->htmlButton("btScan","Ouvrir","info");
-			$bt->setProperty("data-ajax", "Scan/index/".$disque->getId());
-			$this->jquery->getOnClick("a.btn, button.btn","","#content",array("attr"=>"data-ajax"));
 
-			$liste=$this->jquery->bootstrap()->htmlListgroup("list"+$cpt,array(
-				$disque->getNom(),
-				$this->jquery->bootstrap()->htmlProgressbar("progress"+$cpt,$style,$usage*100)->setStriped(true)->showCaption(true),//->setStyleLimits(array("info"=>10,"success"=>50,"warning"=>80,"danger"=>100))
-				$bt
-			));
-			$badge=round(($occupation/ModelUtils::sizeConverter($tarif->getUnite())),2)." ".$tarif->getUnite()." / ".$tarif->getQuota()." ".$tarif->getUnite();
-			$liste->getItem(0)->addBadge($badge);
-			$listGroup->addItem($liste);
+			$listGroup=$this->jquery->bootstrap()->htmlListgroup("list",array());
+			foreach($disquesUser as $disque){
+				$occupation=ModelUtils::getDisqueOccupation($this->config->cloud,$disque);
+				$tarif=Tarif::findFirst(ModelUtils::getDisqueTarif($disque));
+				$usage=round((($occupation/ModelUtils::sizeConverter($tarif->getUnite()))/$tarif->getQuota()),2);
+				if($usage<0.1)$style="info";
+				else if($usage<0.5)$style="success";
+				else if($usage<0.8)$style="warning";
+				else $style="danger";
 
-			$cpt++;
+				$bootstrap=$this->jquery->bootstrap();
+				$bt=$bootstrap->htmlButton("btScan","Ouvrir","primary")->setProperty("data-ajax", "Scan/".$disque->getId());
+				$this->jquery->getOnClick("a.btn, button.btn","","#content",array("attr"=>"data-ajax"));
+
+				$liste=$this->jquery->bootstrap()->htmlListgroup("list"+$cpt,array(
+					$disque->getNom(),
+					$this->jquery->bootstrap()->htmlProgressbar("progress"+$cpt,$style,$usage*100)->setStriped(true)->showCaption(true),//->setStyleLimits(array("info"=>10,"success"=>50,"warning"=>80,"danger"=>100))
+				));
+				$liste->addItem($bt);
+				$badge=round(($occupation/ModelUtils::sizeConverter($tarif->getUnite())),2)." ".$tarif->getUnite()." / ".$tarif->getQuota()." ".$tarif->getUnite();
+				$liste->getItem(0)->addBadge($badge);
+				$listGroup->addItem($liste);
+
+				$cpt++;
+			}
+			$this->view->setVars(array("userCo"=>true,"liste"=>$listGroup,"disquesUser"=>$disquesUser,"user"=>$user));
+
+			$this->jquery->compile($this->view);
+		}else {
+			$this->view->setVars(array("userCo"=>false));
+			$this->jquery->compile($this->view);
 		}
-		$this->view->setVars(array("liste"=>$listGroup,"disquesUser"=>$disquesUser,"user"=>$user));
-		$this->jquery->compile($this->view);
 
 	}
 }
--- app/views/MyDisques/index.volt
+++ app/views/MyDisques/index.volt
@@ -1,3 +1,8 @@
+{% if userCo %}
 <h1>Mes disques -> {{ user.login }} ({{ user.prenom }} {{ user.nom }})</h1>
     {{ liste }}
+{% else %}
+    Vous devez vous connecter
+{% endif %}
+
 
